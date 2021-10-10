9856d80115af6c899e9773b50e119a2dcb74d588
sryoya <sryoya0814@gmail.com>
2021-10-10 21:05:08 +0900

    back to float calc version in decibi

--- decibi/main.go
+++ decibi/main.go
@@ -7,39 +7,19 @@ import (
 
 func main() {
 	num := 0.625
-	decimalDegit := 3
-	shift := math.Pow10(decimalDegit)
 
-	remain := num * shift
-	// var res []bool
+	remain := num
 	for {
 		double := remain * 2
 
-		// 切り捨て
-		val := math.Floor(double / shift)
+		interger, frac := math.Modf(double)
 
-		fmt.Print(val)
-		remain = double - val*shift
-
-		// interger, frac := math.Modf(double)
-
-		// fmt.Println(interger)
-		// remain = math.Round(frac*100) / 100
+		fmt.Print(interger)
+		remain = math.Round(frac*100) / 100
 
 		if remain == 0 {
 			fmt.Println("break")
 			break
 		}
-	}
-}
-
-func printBinary(bs []bool) {
-	for _, b := range bs {
-		if b {
-			fmt.Print(1)
-			continue
-		}
-
-		fmt.Print(0)
 	}
 }
